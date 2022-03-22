357d15d886e2aa94256da83c7d63ca4960fd74d6
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-03-22 18:42:00 +0800

    add material var and const

---
+++ main.js
@@ -0,0 +1,11 @@
+// ES5
+var myName = "BuildWith Angga";
+console.log(myName);
+
+myName = "BelajarKoding";
+console.log(myName);
+
+// ES6
+const myCar = "Lamborghini";
+myCar = "Pajero"
+console.log(myCar)
