ea9d4f3ffce521ba63aac12eccb912166d9e8551
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-03-25 19:42:08 +0800

    add material inheritance

--- main.js
+++ main.js
@@ -15,7 +15,27 @@ class Siswa {
     join() {
         console.log(this.username + ' telah bergabung pada kelas ' + this.name_kelas);
     }
+
+    countMember() {
+        console.log('Tersedia 100 member');
+    }
+
 }
 
-let addSiswa = new Siswa('bwastudio', '230fsdf', 'Web Design');
-addSiswa.join();
+// let addSiswa = new Siswa('bwastudio', '230fsdf', 'Web Design');
+// addSiswa.join();
+
+class Membership extends Siswa {
+    constructor(username, paket) {
+        super(username);
+        this.paket = paket;
+    }
+
+    joinPackage() {
+        console.log('Hi' + this.username + ' telah langganan paket ' + this.paket);
+    }
+}
+
+let addMember = new Membership('bwastudio', 'Premium');
+addMember.joinPackage();
+addMember.countMember();
